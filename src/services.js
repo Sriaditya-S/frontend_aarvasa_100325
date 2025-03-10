04f4942f34fba131d6fe3cd339ca9eb284950206
Uday Prakash Sahu <115711567+udayprakashsahu@users.noreply.github.com>
2025-03-10 23:48:40 +0530

    Update services.js
    Updated image in services page

--- src/services.js
+++ src/services.js
@@ -26,7 +26,7 @@ const Services = () => {
         </div>
       </div>
       <div >
-        <img src="/services/home.png" alt="home"></img>
+        <img src="/3d-rendering-isometric-fdgdf 1.png" alt="home"></img>
         </div>  
      </div>
      <div>
