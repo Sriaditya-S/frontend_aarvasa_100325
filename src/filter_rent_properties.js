4512a0771219fbb0d305dec87e3c75c0ba274691
LakshayMohan <lakshaymohan.21@gmail.com>
2025-04-15 16:17:52 +0530

    testing new route for fetching all rent properties

--- src/filter_rent_properties.js
+++ src/filter_rent_properties.js
@@ -45,7 +45,7 @@ function RPF(){
 
         async function t(){
 
-            let op = await fetch('https://backendaarvasa100325.vercel.app/all_rent_properties',
+            let op = await fetch('https://backendaarvasa100325.vercel.app/properties/all_rent',
                 {
                     method: 'POST',
                     headers: {
